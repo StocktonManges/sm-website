43fbc6ff7055aaa10cc9c0270c936c0c02ddf33a
StocktonManges <stocktonmanges@gmail.com>
2023-07-24 20:58:58 -0600

    createPortfolioCards function completed.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -26,6 +26,41 @@ const currentTheme = localStorage.getItem(theme);
 
 /* Portfolio */
 const filterLink = document.querySelectorAll(dataFilter);
+// Using 'querySelector' to select an id:
+const searchBox = document.querySelector('#search');
+// Array for createPortfolioCards function.
+const portfolioCardData = [
+  // data-attribute - image source - div contents - h3 contents
+  ['web', "./assets/images/portfolio-1.jpg", 'Web Development', 'Food Website'],
+  ['web', "./assets/images/portfolio-2.jpg", 'Web Development', 'Skate Website'],
+  ['web', "./assets/images/portfolio-3.jpg", 'Web Development', 'Eating Website'],
+  ['ui', "./assets/images/portfolio-4.jpg", 'UI Design', 'Cool Design'],
+  ['app', "./assets/images/portfolio-5.jpg", 'App Development', 'Game App'],
+  ['app', "./assets/images/portfolio-6.jpg", 'App Development', 'Gambling App'],
+  ['app', "./assets/images/portfolio-7.jpg", 'App Development', 'Money App'],
+  ['ui', "./assets/images/portfolio-8.jpg", 'UI Design', 'Fantastic Design'],
+];
+
+// Create each portfolio card
+function createPortfolioCards(array) {
+  for (const arr of array) {
+    document.querySelector('.portfolio-grid').innerHTML += `
+    <div class="portfolio-card" data-item=${arr[0]}>
+      <div class="card-body">
+        <img src=${arr[1]} alt="portfolio icon">
+        <a href="#" class="card-popup-box">
+          <div>${arr[2]}</div>
+          <h3>${arr[3]}</h3>
+        </a>
+      </div>
+    </div>`
+  }
+}
+createPortfolioCards(portfolioCardData);
+/* The Node List of all the 'data-item' values must be created after
+createPortfolioCards is called because there are no elements with the
+'data-item' attribute beforehand, thus negating the ability to filter
+using data attributes. */
 const portfolioItems = document.querySelectorAll(portfolioData);
 
 /* Modal */
@@ -52,8 +87,8 @@ const setTheme = (val) => {
   }
 };
 
-// Checks the current theme when the page loads and puts the 'active'
-// class on the appropriate theme button.
+/* Checks the current theme when the page loads and puts the 'active'
+class on the appropriate theme button. */
 if (currentTheme) {
   root.setAttribute(dataTheme, currentTheme);
   switcher.forEach((btn) => {
@@ -85,8 +120,22 @@ for (const elm of switcher) {
   });
 }
 
-// Adds the 'active' class to the portfolio filter headings when clicked
-// and displays the appropriate cards.
+// Portfolio
+searchBox.addEventListener('keyup', e => {
+  // This assigns the value inside the search bar to 'searchInput'.
+  const searchInput = e.target.value.toLowerCase().trim(); // .trim() removes spaces.
+  portfolioItems.forEach((card) => {
+    // If any of the data attributes include the searched value then display it.
+    if (card.dataset.item.includes(searchInput)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  })
+});
+
+/* Adds the 'active' class to the portfolio filter headings when clicked
+and displays the appropriate cards. */
 for (const link of filterLink) {
   link.addEventListener('click', function() {
     setActive(link, '.filter-link')
@@ -96,8 +145,8 @@ for (const link of filterLink) {
       if (filter === 'all') {
         card.style.display = 'block';
       } else if (card.dataset.item === filter) {
-        // This adds the 'display: block' attribute to the card if the
-        // data attribute matches the filtered attribute.
+        /* This adds the 'display: block' attribute to the card if the
+        data attribute matches the filtered attribute. */
         card.style.display = 'block';
       } else {
         card.style.display = 'none';
